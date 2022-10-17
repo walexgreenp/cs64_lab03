5080847c1a8ec9d381538bf9068575e2186f651d
William <wgreenwald@ucsb.edu>
2022-10-16 23:41:04 -0700

    hello.asm passing personal tests

--- hello.asm
+++ hello.asm
@@ -4,13 +4,43 @@
 #  Data Area - allocate and initialize variables
 .data
 	# TODO: Write your string definitions here
+	enter_text: .asciiz "Enter an integer: "
 
 #Text Area (i.e. instructions)
 .text
 main:
-
 	# TODO: Write your code here
+	# Print string
+	li $v0 4
+	la $a0 enter_text
+	syscall
+
+	# Getting user input
+	li $v0 5
+	syscall
+	move $t0 $v0
+
+	li $t1 1
+	and $t2 $t0 $t1
+
+	bne $t2 $0 odd
+
+even:
+	add $t2 $t0 $t0
+	add $t0 $t2 $t0
+	j print
+
+odd:
+	add $t0 $t0 $t0
+	j print
+
+print:
+	li $v0 1
+	move $a0 $t0
+	syscall
 
 exit:
 	# Exit SPIM: Write your code here!
+	li $v0 10
+	syscall
 
